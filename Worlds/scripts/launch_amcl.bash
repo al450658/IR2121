ed65537be590c5d88580d9faa8c1afff74f87b00
al450658 <al450658@uji.es>
2026-09-21 18:54:59 +0200

    se ha añadido worlds

---
+++ Worlds/scripts/launch_amcl.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /opt/ros/humble/setup.bash
+export ROS_LOCALHOST_ONLY=1
+export TURTLEBOT3_MODEL=burger
+
+ros2 launch amcl.launch.py \
+  use_sim_time:=True \
+  map:=../TI_n1_edited.yaml
+
